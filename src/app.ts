7499eef488825f9b6b0edad770462a22b6dfb730
Cleberson F R Junior <cleberson.dev@gmail.com>
2020-09-02 11:23:17 -0300

    Ran both servers

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,7 @@
-console.log('Hello, world!');
+import app from './http';
+import { runServer as runWSS } from './ws';
+
+const HTTP_PORT = Number(process.env.HTTP_PORT || 5000);
+
+app.listen(HTTP_PORT, () => console.log('Running HTTP server on port ' + HTTP_PORT));
+runWSS();
