636f8d84b7303d9eae8b696045d9c12e8a5364ba
Cleberson F R Junior <cleberson.dev@gmail.com>
2020-09-02 11:33:29 -0300

    Served static files on 'src/static/' folder

--- src/http.ts
+++ src/http.ts
@@ -3,10 +3,15 @@ import path from 'path';
 
 const app = express();
 
-const viewsDir = path.join(process.cwd(), 'src', 'views');
+const directories = {
+  views: path.join(process.cwd(), 'src', 'views'),
+  static: path.join(process.cwd(), 'src', 'static'),
+}
 
 app.set('view engine', 'pug');
-app.set('views', viewsDir);
+app.set('views', directories.views);
+
+app.use('/static', express.static(directories.static));
 
 app.get('/', (_, res) => {
   res.render('index');
